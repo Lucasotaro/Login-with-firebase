3d409b577f3990732ba9aebae40af7d9bb91e14e
Lucas <zlucas.np@gmail.com>
2022-03-08 15:12:48 -0300

    visual loading feedback

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,30 @@
 login.onsubmit = function(event){
+    document.getElementById("loading").style.display = "block"
+
     event.preventDefault()
-    if(login.btn_lg.innerHTML == "Login"){
-        firebase.auth().signInWithEmailAndPassword(login.email.value, login.password.value).then(function(user){
-            console.log("Acessou com sucesso")
-            console.log(user)
-        }).catch(function(error){
+    if(login.btn_lg.innerHTML == "Login"    ){
+        firebase.auth().signInWithEmailAndPassword(login.email.value, login.password.value).catch(function(error){
             console.log("Falha no acesso")
             console.log(error)
         })
     }
 
     else{
-        firebase.auth().createUserWithEmailAndPassword(login.email.value, login.password.value).then(function(user){
-            console.log("Cadastrou com sucesso")
-            console.log(user)
-        }).catch(function(error){
+        firebase.auth().createUserWithEmailAndPassword(login.email.value, login.password.value).catch(function(error){
             console.log("Falha no cadastro")
             console.log(error)
         })
     }
 }
+
+firebase.auth().onAuthStateChanged(function(user){
+    document.getElementById("loading").style.display = "none"
+    
+    if(user){
+        console.log("Usuário autenticado")
+        console.log(user)
+    }
+    else{
+        console.log("Usuário não autenticado")
+    }
+})
